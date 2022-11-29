7031775d23b4e72e9deaad70fcf369760d628fb9
Christian Darvin <110832080+christiandarvs@users.noreply.github.com>
2022-11-30 00:06:03 +0800

    Add files via upload

---
+++ comments.dart
@@ -0,0 +1,12 @@
+void main(List<String> args) {
+  //Single Line Comment
+  print('Hello World');
+
+  /* This is a
+      Multi Line Comment
+   */
+  print('Dart Programming');
+
+  /// Documentation Comment
+  print('Flutter Development');
+}
