149ff0f71d67b4b128a10dc6be4c6e6268168ff8
Christian Darvin <110832080+christiandarvs@users.noreply.github.com>
2022-11-29 23:32:02 +0800

    Add files via upload

---
+++ variables.dart
@@ -0,0 +1,22 @@
+void main(List<String> args) {
+  var name = 'Christian Elijah Darvin';
+  var age = 18;
+  var course = 'Bachelor of Science in Computer Science';
+  var faveProgLang = 'Dart';
+  var gpa = 95.5;
+  var isContentCreator = true;
+  var isProgrammer = true;
+  const pi = 3.141592653589793238462643;
+
+  print('--------Personal Information---------');
+  print('');
+  print('Name: $name');
+  print('Age: $age');
+  print('Course: $course');
+  print('Favorite Programming Language: $faveProgLang');
+  print('GPA: $gpa');
+  print('Content Creator? $isContentCreator');
+  print('Programmer? $isProgrammer');
+  print('Value of pi: $pi');
+  print('');
+}
